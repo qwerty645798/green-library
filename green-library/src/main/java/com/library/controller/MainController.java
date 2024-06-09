3321e4068078e2127009bafb5fafa674f0968ed6
qwerty645798 <harry980523@naver.com>
2024-06-09 16:03:13 +0900

    Update MainController.java

--- green-library/src/main/java/com/library/controller/MainController.java
+++ green-library/src/main/java/com/library/controller/MainController.java
@@ -8,6 +8,11 @@ public class MainController {
 
 	@GetMapping("/")
 	public String home () {
-		return "home";
+		return "index";
+	}
+	
+	@GetMapping("dataRule")
+	public String dataRule() {
+		return "dataRule";
 	}
 }
