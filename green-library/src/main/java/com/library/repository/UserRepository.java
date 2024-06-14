92d457b61fbcd03a1c5ba74d602b6b166df21cec
qwerty645798 <harry980523@naver.com>
2024-06-14 16:51:04 +0900

    .

--- green-library/src/main/java/com/library/repository/UserRepository.java
+++ green-library/src/main/java/com/library/repository/UserRepository.java
@@ -1,12 +1,35 @@
 package com.library.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.library.dto.UserDto;
 
 @Repository
 public class UserRepository {
-	
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public UserDto findByUserId(String userId) {
+        String sql = "SELECT * FROM users WHERE user_id = ?";
+        return jdbcTemplate.queryForObject(sql, new RowMapper<UserDto>() {
+            @Override
+            public UserDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                UserDto user = new UserDto();
+                user.setUserId(rs.getString("user_id"));
+                user.setUserPass(rs.getString("user_pass"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setPhone(rs.getString("phone"));
+                user.setBirth(rs.getString("birth"));
+                user.setOverdueCount(rs.getInt("overdue_count"));
+                user.setSuspended(rs.getString("suspended").charAt(0));
+                return user;
+            }
+        }, userId);
+    }
 }
