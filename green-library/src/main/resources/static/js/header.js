4b238bb79f82de9ef39a5c3a9425a38f2d79198f
qwerty645798 <harry980523@naver.com>
2024-06-17 14:33:14 +0900

    .

--- green-library/src/main/resources/static/js/header.js
+++ green-library/src/main/resources/static/js/header.js
@@ -60,7 +60,7 @@
 
     let subMenuTop = document.getElementById('subMenuTop').children;
     let subMenu = document.getElementById('subMenu').children;
-    
+    let input = document.getElementsByTagName("input");
 function accordian(){
     for(let i=0;i<5;i++){
 
@@ -69,13 +69,23 @@ function accordian(){
                 subMenu[i].style.display = "flex";
                 subMenu[i].style.height =  subMenu[i].scrollHeight + "px";
                 subMenu[i].style.transition = "height 300ms ease-in-out";
+                subMenu[i].style.zIndex = "10";
+                for(let i=0; i<input.length; i++){
+					input[i].zIndex = "3";
+				}
+                
             }else if(subMenu[i].scrollHeight!=0){
                 subMenu[i].style.display = "none";
                 subMenu[i].style.height =  subMenu[i].scrollHeight + "px";
                 subMenu[i].style.transition = "height 300ms ease-in-out";
+                subMenu[i].style.zIndex = "3";
+                for(let i=0; i<input.length; i++){
+					input[i].zIndex = "10";
+				}
             }
         }
     }
+    
 }
 
 window.onload = accordian();
