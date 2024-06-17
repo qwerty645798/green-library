07b51a3cd00de16e17b9e01a3735921eb4e75229
davidtwo000 <davidtwo3208@gmail.com>
2024-06-17 10:38:39 +0900

    david - .

--- green-library/src/main/java/com/library/controller/BookDetailController.java
+++ green-library/src/main/java/com/library/controller/BookDetailController.java
@@ -1,15 +1,26 @@
 
 package com.library.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.library.dto.BookDetailDto;
+import com.library.service.BookDetailService;
 
 
 @Controller
 public class BookDetailController {
 	
+	@Autowired
+	private BookDetailService bookDetailService;
+	
     @GetMapping("/bookDetail")
-    public String bookDeatil() {
-    	return "bookDeatil";
+    public String bookDetail(@RequestParam("bookId") String bookId, Model model) {
+    	BookDetailDto bookDetail = bookDetailService.getBookDetail(bookId);
+    	model.addAttribute("book", bookDetail);
+    	return "bookDetail";
     }
 }
--- green-library/src/main/java/com/library/service/BookDetailService.java
+++ green-library/src/main/java/com/library/service/BookDetailService.java
@@ -1,12 +1,9 @@
 package com.library.service;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.library.dto.BookDetailDto;
-import com.library.dto.UserDto;
 import com.library.repository.BookDetailRepository;
 
 @Service
@@ -15,17 +12,8 @@ public class BookDetailService {
  	@Autowired
  	private BookDetailRepository bookDetailRepository;
  
- 	@Override
-	public BookDetails loadBookByBookId(String bookId) throws BookIdNotFoundException {
-	    BookDetailDto bookdetailDto = bookDetailRepository.findByBookId(bookId);
-	    
-	    if (bookdetailDto == null) {
-	        throw new UsernameNotFoundException("User not found");
-	    }
-	    return User.withUsername(userDto.getUserId())
-	               .password(userDto.getUserPass())
-	               .authorities(Collections.emptyList()) // 역할을 설정
-	               .build();
-	}
+ 	public BookDetailDto getBookDetail(String bookId) {
+ 		return bookDetailRepository.findByBookId(bookId);
+ 	}
 	
 }
