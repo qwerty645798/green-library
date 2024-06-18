9ea01b552fff673e2027f9f414cd85f6f7e2529b
qwerty645798 <harry980523@naver.com>
2024-06-18 11:30:21 +0900

    .

--- green-library/src/main/java/com/library/controller/user/UserController.java
+++ green-library/src/main/java/com/library/controller/user/UserController.java
@@ -1,13 +1,13 @@
 package com.library.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.library.dto.user.UserDto;
 import com.library.service.user.UserInfoModificationService;
@@ -16,94 +16,94 @@ import com.library.service.user.UserLoginService;
 
 import jakarta.validation.Valid;
 
-
 @Controller
 //@RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserLoginService userLoginService;
 
-
-	
 	@GetMapping("/userAgreement")
 	public String userAgreement() {
 		return "user/userAgreement";
 	}
-	
+
 	@GetMapping("/userJoin")
 	public String userJoin() {
 		return "user/userJoin";
 	}
-	
+
 	@GetMapping("/userLogin")
 	public String userLogin() {
 		return "user/userLogin";
 	}
-	
+
 	@GetMapping("/userFinding")
 	public String userFinding() {
 		return "user/userFinding";
 	}
-	
+
 	@GetMapping("/userFindingAlt")
 	public String userFindingAlt() {
 		return "user/userFindingAlt";
 	}
-	
 
-	
-
-	
 	@GetMapping("/userUseInformation")
-	public String userUseInformation() {	
+	public String userUseInformation() {
 		return "user/userUseInformation";
 	}
-	
+
 	@GetMapping("/useInformationBoard")
-	public String useInformationBoard () {
+	public String useInformationBoard() {
 		return "user/useInformation/board";
 	}
-	
+
 	@GetMapping("/userInquiryDetail")
 	public String userInquiryDetail() {
 		return "user/userInquiryDetail";
 	}
-	
+
 	@GetMapping("/userInquiryCreate")
-	public String userInquiryCreate () {
+	public String userInquiryCreate() {
 		return "user/userInquiryCreate";
 	}
-	
 
 	@Autowired
 	private UserInfoService userInfoService;
-	
+
 	@GetMapping("/userInfo")
-	public String userInfo (Model model) {
+	public String userInfo(Model model) {
 		UserDto userDto = userInfoService.getUserDetails();
-    	model.addAttribute("user", userDto);
+		model.addAttribute("user", userDto);
 		return "user/userInfo";
 	}
-	
+
 	@GetMapping("/userInfoModification")
 	public String userInfoModification(Model model) {
 		UserDto userDto = userInfoService.getUserDetails();
-    	model.addAttribute("user", userDto);
+		model.addAttribute("user", userDto);
 		return "user/userInfoModification";
 	}
-	
+
 	@Autowired
 	private UserInfoModificationService userInfoModificationService;
-	
+
 	/*
 	 * @PostMapping("/userInfoModification-perform") public String
-	 * userInfoModificationPerform(@ModelAttribute("user") @Valid UserDto userDto,
-	 * BindingResult result) { if (result.hasErrors()) { return
-	 * "userInfoModification"; }
+	 * userInfoModificationPerform(
 	 * 
-	 * userInfoModificationService.update(userDto); return
-	 * "redirect:/registration?success"; }
+	 * @ModelAttribute("user") @Valid UserDto userDto,
+	 * 
+	 * @RequestParam("emailFront") String ef,
+	 * 
+	 * @RequestParam("emailBack") String eb, BindingResult result ) {
+	 * 
+	 * if (result.hasErrors()) { return "userInfoModification"; }
+	 * 
+	 * userDto.setEmail(ef+"@"+eb); userInfoModificationService.update(userDto);
+	 * return "redirect:/registration?success";
+	 * 
+	 * }
 	 */
 
 }
--- green-library/src/main/java/com/library/repository/user/InfoModificationRepository.java
+++ green-library/src/main/java/com/library/repository/user/InfoModificationRepository.java
@@ -1,5 +1,6 @@
 package com.library.repository.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +9,13 @@ import com.library.dto.user.UserDto;
 @Repository
 public class InfoModificationRepository {
 
-	/*@Autowird
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	public int updateUserInfo(UserDto userDto) {
-        String sql = "UPDATE users SET userName = ?, userEmail = ? WHERE id = ?";
-        return jdbcTemplate.update(sql, userDto.getUserName(), user.getUserEmail(), user.getId());
-    }*/
+
+	/*
+	 * public int updateUserInfo(UserDto userDto) { String sql =
+	 * "UPDATE users SET userName = ?, userEmail = ? WHERE id = ?"; return
+	 * jdbcTemplate.update(sql, userDto.getUserName(), user.getUserEmail(),
+	 * user.getId()); }
+	 */
 }
