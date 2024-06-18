20c19097484b44aff13e629aba56a798b2f732df
davidtwo000 <davidtwo3208@gmail.com>
2024-06-18 09:50:24 +0900

    .

--- green-library/src/main/java/com/library/repository/assets/PopularBookRepository.java
+++ green-library/src/main/java/com/library/repository/assets/PopularBookRepository.java
@@ -9,7 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import com.library.dto.assets.InitiativeBookDto;
+import com.library.dto.assets.PopularBookDto;
 
 @Repository
 public class PopularBookRepository {
@@ -17,15 +17,37 @@ public class PopularBookRepository {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	public List<InitiativeBookDto> findBookId() {
-		String sql = "Select book_id, img from (select book_id, img from books order by publication_date desc) where rownum<= 6";
+	public List<PopularBookDto> findBookId() {
+		String sql = "Select b.book_id, b.img, b.title, a.author_name, p.publisher_name, b.availability "
+				+ "from ("
+				+ "select b.book_id, b.img, b.title, a.author_name, p.publisher_name, b.availability "
+				+ "from books b "
+				+ "JOIN authors a ON b.author_id = a.author_id"
+				+ "JOIN publishers p b.publisher_id = p.publisher_id  "
+				+ "order by b.borrow_count desc) "
+				+ "where rownum<= 30";
 		
-		return jdbcTemplate.query(sql, new RowMapper<InitiativeBookDto>() {
+		return jdbcTemplate.query(sql, new RowMapper<PopularBookDto>() {
 			@Override
-			public InitiativeBookDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-				InitiativeBookDto book = new InitiativeBookDto();
+			public PopularBookDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+				PopularBookDto book = new PopularBookDto();
                 book.setBook_id(rs.getInt("book_id"));
                 book.setImg(rs.getString("img"));
+                book.setTitle(rs.getString("title"));
+                book.setAuthorName(rs.getString("author_name"));
+                book.setPublisherName(rs.getString("publisher_name"));
+                book.setAvailability(rs.getString("availability"));  
+                                
+                if (book.getAvailability() != null) {
+                    if (book.getAvailability().equals("1")) {
+                        book.setAvailability("대출가능");
+                    } else {
+                        book.setAvailability("대출불가");
+                    }
+                } else {
+                    book.setAvailability("알수없음");
+                }
+                
 				return book;
 			}
 		});
