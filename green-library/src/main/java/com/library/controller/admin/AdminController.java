0158d084003d298230679fd46aaa47b772fc5fbc
ys389164 <ys389164@gmail.com>
2024-06-21 13:20:20 +0900

    20240621

--- green-library/src/main/java/com/library/controller/admin/AdminController.java
+++ green-library/src/main/java/com/library/controller/admin/AdminController.java
@@ -1,7 +1,39 @@
 package com.library.controller.admin;
 
+import com.library.dto.admin._normal.AdminDTO;
+import com.library.service.admin.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public AdminDTO loginAdmin(@RequestParam String adminId, @RequestParam String adminPass) {
+        return adminService.loginAdmin(adminId, adminPass);
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<AdminDTO> allAdminManage() {
+        return adminService.allAdminManage();
+    }
+
+    @GetMapping("/{adminId}")
+    @ResponseBody
+    public AdminDTO findAdminById(@PathVariable String adminId) {
+        return adminService.findAdminById(adminId);
+    }
 }
--- green-library/src/main/java/com/library/controller/admin/BookController.java
+++ green-library/src/main/java/com/library/controller/admin/BookController.java
@@ -1,8 +1,81 @@
 package com.library.controller.admin;
 
+import com.library.dto.admin._normal.BookDTO;
+import com.library.service.admin.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller("AdminBookController")
+@RequestMapping("/admin/books")
 public class BookController {
 
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<BookDTO> getAllBooks() {
+        return bookService.allHavingBookManage();
+    }
+
+    @GetMapping("/title/{title}")
+    @ResponseBody
+    public List<BookDTO> getBooksByTitle(@PathVariable String title) {
+        return bookService.findBookByTitle(title);
+    }
+
+    @GetMapping("/author/{author}")
+    @ResponseBody
+    public List<BookDTO> getBooksByAuthor(@PathVariable String author) {
+        return bookService.findBookByAuthor(author);
+    }
+
+    @GetMapping("/genre/{genre}")
+    @ResponseBody
+    public List<BookDTO> getBooksByGenre(@PathVariable String genre) {
+        return bookService.findBookByGenre(genre);
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public void createBook(@RequestBody BookDTO book) {
+        bookService.createBook(book);
+    }
+
+    @PutMapping("/update")
+    @ResponseBody
+    public void updateBook(@RequestBody BookDTO book) {
+        bookService.updateBook(book);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public void deleteBook(@PathVariable int id) {
+        bookService.deleteBook(id);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public BookDTO getBookById(@PathVariable int id) {
+        return bookService.getBookById(id);
+    }
+
+    @GetMapping("/previous/{id}")
+    @ResponseBody
+    public String previousBook(@PathVariable int id) {
+        return bookService.previousBook(id);
+    }
+
+    @GetMapping("/next/{id}")
+    @ResponseBody
+    public String nextBook(@PathVariable int id) {
+        return bookService.nextBook(id);
+    }
 }
