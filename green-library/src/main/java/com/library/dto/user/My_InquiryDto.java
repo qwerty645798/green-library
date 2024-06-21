06e60dfbba14465a9f90670c2dc3608598d8f2da
davidtwo000 <davidtwo3208@gmail.com>
2024-06-21 10:00:09 +0900

    david - 스키마

--- green-library/src/main/java/com/library/dto/user/My_InquiryDto.java
+++ green-library/src/main/java/com/library/dto/user/My_InquiryDto.java
@@ -4,13 +4,43 @@ import java.sql.Date;
 
 public class My_InquiryDto {
 	
-	/*
 	private int inquiryId;
 	private String userId;
-	private Date 작성일자;
+	private Date inquiryDate;
 	private String inquiryTitle;
-	private 형태 답변상태;
-	*/
+	private String responserTF;
+	
+	public int getInquiryId() {
+		return inquiryId;
+	}
+	public void setInquiryId(int inquiryId) {
+		this.inquiryId = inquiryId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Date getInquiryDate() {
+		return inquiryDate;
+	}
+	public void setInquiryDate(Date inquiryDate) {
+		this.inquiryDate = inquiryDate;
+	}
+	public String getInquiryTitle() {
+		return inquiryTitle;
+	}
+	public void setInquiryTitle(String inquiryTitle) {
+		this.inquiryTitle = inquiryTitle;
+	}
+	public String getResponserTF() {
+		return responserTF;
+	}
+	public void setResponserTF(String responserTF) {
+		this.responserTF = responserTF;
+	}
+	
 	
 	
 }
