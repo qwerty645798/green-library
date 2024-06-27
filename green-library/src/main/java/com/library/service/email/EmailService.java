8b8a9733da5109c517194e4b1d17234b2137b18c
qwerty645798 <harry980523@naver.com>
2024-06-27 12:50:06 +0900

    harry

---
+++ green-library/src/main/java/com/library/service/email/EmailService.java
@@ -0,0 +1,37 @@
+package com.library.service.email;
+
+import java.io.UnsupportedEncodingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+	
+	private final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+	@Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String content) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom("GreenLibrary@gmail.com", "Green Library");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            mailSender.send(message);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            logger.warn("failed to send email - to : ", to, e);
+        }
+    }
+}
