b767eeff0f66c7440b7c753f719526111f670500
qwerty645798 <163392521+qwerty645798@users.noreply.github.com>
2024-06-28 09:51:12 +0900

    Revert "header js css oftenAsk js 수정"

--- green-library/src/main/resources/static/js/userInfo.js
+++ green-library/src/main/resources/static/js/userInfo.js
@@ -18,36 +18,26 @@ cancel.addEventListener('click', () => {
 
 /*본래 userInfo랑 userUseInformation 맨 아래에 있던 거 옮겨옴 */
 document.addEventListener('DOMContentLoaded', function() {
-        fetchDataAndSendToIframe('rentHistory');
-    });
-    
-    document.querySelectorAll('.Dheader').forEach(header => {
-        header.addEventListener('click', function() {
-            let condition = this.getAttribute('data-condition');
-            fetchDataAndSendToIframe(condition);
+            fetchDataAndSendToIframe('rentHistory');
         });
-    });
+        
+        document.querySelectorAll('.Dheader').forEach(header => {
+            header.addEventListener('click', function() {
+                let condition = this.getAttribute('data-condition');
+                fetchDataAndSendToIframe(condition);
+            });
+        });
 
-    function initialLoadIframe() {
-        fetchDataAndSendToIframe('rentHistory');
-    }
-
-    function iframeHeight(condition, data) {
-        let iframe = document.getElementById('iframe');
-        if (data.length < 10) {
-            iframe.style.height = 720 - (10 - data.length) * 34 + "px";
-        } else {
-            iframe.style.height = "720px";
+        function initialLoadIframe() {
+            fetchDataAndSendToIframe('rentHistory');
         }
-    }
 
-    function fetchDataAndSendToIframe(condition) {
-        fetch(`/getUserData`)
-            .then(response => response.json())
-            .then(data => {
-                let iframe = document.getElementById('iframe');
-                iframe.contentWindow.postMessage({ condition: condition, data: data[condition] }, '*');
-                iframeHeight(condition, data[condition]);
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }
+        function fetchDataAndSendToIframe(condition) {
+            fetch(`/getUserData`)
+                .then(response => response.json())
+                .then(data => {
+                    let iframe = document.getElementById('iframe');
+                    iframe.contentWindow.postMessage({ condition: condition, data: data[condition] }, '*');
+                })
+                .catch(error => console.error('Error fetching data:', error));
+        }
